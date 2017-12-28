fef77b7cc80902e7b78a398f9c50c149e1c3dc9a
limin1994 <171313299@qq.com>
2017-12-28 12:55:35 +0800

    测试类提交

--- user_book/src/test/java/com/gloudtek/test/AppVersionServiceTest.java
+++ user_book/src/test/java/com/gloudtek/test/AppVersionServiceTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+
 @ContextConfiguration({ "classpath:spring-renren.xml" })
 public class AppVersionServiceTest {
 
